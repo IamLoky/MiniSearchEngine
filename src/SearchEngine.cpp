e0a2d678c22065f9922ae441884a6607d5555778
IamLoky <lokeshkmoond332311@gmail.com>
2026-06-03 18:18:41 +0530

    Update SearchEngine.cpp

--- src/SearchEngine.cpp
+++ src/SearchEngine.cpp
@@ -1,6 +1,35 @@
 #include "SearchEngine.h"
-#include <iostream>
 
-void SearchEngine::search() {
-    std::cout << "Searching..." << std::endl;
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+string normalizeWord(string word)
+{
+    for(char& ch : word)
+    {
+        ch = tolower(static_cast<unsigned char>(ch));
+    }
+
+    return word;
+}
+
+string cleanWord(string word)
+{
+    string result;
+
+    for(char ch : word)
+    {
+        if(isalnum(static_cast<unsigned char>(ch)))
+        {  
+            result += tolower(ch);
+        }
+    }
+
+    return result;
 }
