96decceb58c369487bb468539a4fac954f4f26c2
IamLoky <lokeshkmoond332311@gmail.com>
2026-07-02 09:27:40 +0530

    Update TextProcessor.h

--- src/TextProcessor.h
+++ src/TextProcessor.h
@@ -1,1 +1,23 @@
+#ifndef TEXT_PROCESSOR_H
+#define TEXT_PROCESSOR_H
 
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+class TextProcessor
+{
+private:
+    std::unordered_set<std::string> stopWords;
+
+public:
+    TextProcessor();
+
+    std::string cleanWord(const std::string& word);
+
+    bool isStopWord(const std::string& word) const;
+
+    std::vector<std::string> tokenize(const std::string& text);
+};
+
+#endif
