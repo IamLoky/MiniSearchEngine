9f429fa869b2bff5c470cb815a4eec00e68781fb
IamLoky <lokeshkmoond332311@gmail.com>
2026-07-10 20:33:52 +0530

    Update QueryProcessor.cpp

--- src/QueryProcessor.cpp
+++ src/QueryProcessor.cpp
@@ -1,1 +1,10 @@
 #include "QueryProcessor.h"
+
+QueryProcessor::QueryProcessor(
+    const InvertedIndex& index,
+    size_t totalDocuments
+)
+    : index(index),
+      totalDocuments(totalDocuments)
+{
+}
