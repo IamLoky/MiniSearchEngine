64ecb761c6fee47564dd4105ace46a83e84fed1e
IamLoky <lokeshkmoond332311@gmail.com>
2026-07-13 11:15:51 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,57 @@
 #include "SearchEngine.h"
 
+#include <filesystem>
 #include <iostream>
 
 using namespace std;
+
+namespace fs = std::filesystem;
 
 int main()
 {
     SearchEngine engine;
 
-    engine.loadIndex("index.bin");
+    if(fs::exists("index.bin"))
+    {
+        cout << "Loading index...\n";
+        engine.loadIndex("index.bin");
+    }
+    else
+    {
+        cout << "Building index...\n";
+        engine.buildIndex("docs");
+        engine.saveIndex("index.bin");
+    }
 
     string query;
 
-    cout << "Enter search query: ";
-    getline(cin, query);
-
-    auto results = engine.search(query);
-
-    cout << "Search Results:\n";
-
-    for(const auto& result : results)
+    while(true)
     {
-        cout
-            << result.documentName
-            << " ("
-            << result.score
-            << ")\n";
+        cout << "\nEnter search query (type 'exit' to quit): ";
+
+        getline(cin, query);
+
+        if(query == "exit")
+            break;
+
+        auto results = engine.search(query);
+
+        cout << "\nSearch Results:\n";
+
+        if(results.empty())
+        {
+            cout << "No matching documents found.\n";
+            continue;
+        }
+
+        for(const auto& result : results)
+        {
+            cout
+                << result.documentName
+                << " ("
+                << result.score
+                << ")\n";
+        }
     }
 
     return 0;
